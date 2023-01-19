1d9d11a94749a6576deec155311bee2433aea851
Mostafa Jahani <mm.mostafa.mm@gmail.com>
2023-01-20 00:33:32 +0330

    Refactor code: fetch all data and handle error and loading in HomePage with RTKQuery

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,23 +1,39 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import Loading from "./components/layouts/Loading";
+import {usePostsQuery} from './redux/postApi'
+import App from "./App";
 
 const HomePage = () => {
 
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    // const [posts, setPosts] = useState([]);
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState(null);
+    //
+    // useEffect(() => {
+    //     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+    //         setPosts(response.data)
+    //         setLoading(false)
+    //         setError(null)
+    //     }).catch(err => {
+    //         setError(err.message)
+    //         setLoading(false)
+    //     })
+    // }, []);
 
-    useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-            setPosts(response.data)
-            setLoading(false)
-            setError(null)
-        }).catch(err => {
-            setError(err.message)
-            setLoading(false)
-        })
-    }, []);
+    const {data: posts, isError, error, isLoading, isSuccess} = usePostsQuery()
+
+    if (isError) {
+        return (
+            <div className="m-5">
+                <div className="fw-bold text-danger">{error.status}</div>
+                <div>{error.error}</div>
+            </div>
+        )
+    }
+
+    if (isLoading) return <Loading />
+
 
 
     return (
@@ -29,8 +45,7 @@ const HomePage = () => {
                     <form>
                         <div className="d-flex gap-3">
                             <div>
-                                <input type="text" name="search_input" id="search_input"
-                                       className="form-control flex-grow-1" placeholder="type every word..."/>
+                                <input type="text" name="search_input" id="search_input" className="form-control flex-grow-1" placeholder="type every word..."/>
                             </div>
                             <div>
                                 <button type="submit" className="form-control btn btn-primary">Search</button>
@@ -41,46 +56,47 @@ const HomePage = () => {
                 {/* Search Form */}
 
 
-                {error && <div>{error}</div>}
-                {loading && <Loading/>}
-                {/* Table */}
-                <div className="w-100 mt-3 p-3">
-                    {posts &&
-                        <table className="table table-striped table-hover">
-                            <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Body</th>
-                            </tr>
-                            </thead>
+                {isSuccess &&
+                    <>
+                        {/*Table*/}
+                        <div className="w-100 mt-3 p-3">
+                            {posts &&
+                                <table className="table table-striped table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Title</th>
+                                        <th>Body</th>
+                                    </tr>
+                                    </thead>
 
-                            <tbody>
-                            {posts && posts.map(post => (
-                                <tr key={post.id}>
-                                    <td>{post.id}</td>
-                                    <td>{`${post.title.substring(0, 60)}...`}</td>
-                                    <td>{`${post.body.substring(0, 60)}...`}</td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    }
-                </div>
-                {/* Table */}
+                                    <tbody>
+                                    {posts && posts.map(post => (
+                                        <tr key={post.id}>
+                                            <td>{post.id}</td>
+                                            <td>{`${post.title.substring(0, 60)}...`}</td>
+                                            <td>{`${post.body.substring(0, 60)}...`}</td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </table>
+                            }
+                        </div>
+                        {/*Table*/}
 
-
-                {/* Pagination */}
-                <nav className="d-flex justify-content-center">
-                    <ul className="pagination">
-                        <li className="page-item"><a className="page-link" href="#">1</a></li>
-                        <li className="page-item active" aria-current="page">
-                            <span className="page-link">2</span>
-                        </li>
-                        <li className="page-item"><a className="page-link" href="#">3</a></li>
-                    </ul>
-                </nav>
-                {/* Pagination */}
+                        {/*Pagination*/}
+                        <nav className="d-flex justify-content-center">
+                            <ul className="pagination">
+                                <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                <li className="page-item active" aria-current="page">
+                                    <span className="page-link">2</span>
+                                </li>
+                                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                            </ul>
+                        </nav>
+                        {/*Pagination*/}
+                    </>
+                }
 
             </div>
         </>
